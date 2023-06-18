26fc436f68528a31f4cee3ce072713a9b629f3e1
Dima <dimapanteleyvrach@gmail.com>
2023-06-18 22:50:28 +0300

    дз

---
+++ src/main/java/com/example/moki2/Moki2Application.java
@@ -0,0 +1,13 @@
+package com.example.moki2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Moki2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Moki2Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/moki2/Moki2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.moki2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Moki2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
