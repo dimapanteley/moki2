54618673d8d78a32e98a7e4d0e5346c50c2a2be9
Dima <dimapanteleyvrach@gmail.com>
2023-06-18 22:51:54 +0300

    дз

--- src/test/java/com/example/moki2/Moki2ApplicationTests.java
+++ src/test/java/com/example/moki2/Moki2ApplicationTests.java
@@ -2,9 +2,9 @@ package com.example.moki2;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-
+//
 @SpringBootTest
-class Moki2ApplicationTests {
+class  Moki2ApplicationTests {
 
     @Test
     void contextLoads() {
